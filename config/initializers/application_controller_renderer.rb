52517aaeeffdff4ab254edea6ab38def97f96126
wa-r <nyuuuuun14@gmail.com>
2020-11-23 06:21:17 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9b1f695b9dd87d22a1d9f84884467db4e975ecd282dedc189d94938c5bcb7e7630f5b2100f83c324531a484fb4c3d3bcd1da642114a2d8acd3020d1797bd6523'
